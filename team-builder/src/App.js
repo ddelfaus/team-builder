4b5a322614150ad125d3088b604bd5d3422ca2b4
ddelfaus <ddelfaus@gmail.com>
2019-10-16 15:32:32 -0400

    First Commit

---
+++ team-builder/src/App.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+import TeamForm from './Components/TeamForm'
+
+
+
+
+function App() {
+
+  
+
+
+
+  return (
+    <div className="App">
+     <h1>Team Builder!</h1>
+    
+    <TeamForm/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ team-builder/src/Components/TeamForm.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {useState} from 'react';
+
+
+const TeamForm = props => {
+
+
+
+
+return (
+<form>
+    <input
+        name ="name"
+        id = "name"
+        type = "text"
+        placeholder ="name"
+
+    />
+    <input
+        name ="email"
+        id = "email"
+        type = "text"
+        placeholder ="email"
+
+    />
+
+<input
+        name ="role"
+        id = "role"
+        type = "text"
+        placeholder ="role"
+
+    />
+
+
+</form>
+
+)
+
+
+}
+
+
+export default TeamForm;
